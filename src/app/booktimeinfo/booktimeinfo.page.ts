a5a1a8a9e602b740fb559db81bfd32def369ce30
JC <jayceefernandez07@gmail.com>
2019-11-09 19:42:28 +0800

    latest as of november 9, night

--- src/app/booktimeinfo/booktimeinfo.page.ts
+++ src/app/booktimeinfo/booktimeinfo.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-booktimeinfo',
   templateUrl: './booktimeinfo.page.html',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BooktimeinfoPage implements OnInit {
 
+  selectedView = 'datetime';
+
   constructor() { }
 
   ngOnInit() {
   }
+
+
+ 
 
 }
